6a498288a1d74abe71a21d298e0c7d416b429928
Marco <marco@breakthru.dyndns.org>
2014-07-06 11:48:34 +0100

    ...

--- sequential/include/synthdata.h
+++ sequential/include/synthdata.h
@@ -4,13 +4,17 @@
 #include "camera.h"
 #include "shapemodel.h"
 #include <boost/shared_ptr.hpp>
+#include <cstdlib>
 
 void make_synth_data(double *params, boost::shared_ptr<RigidShape<double> > *shape )
 {
     const int num_points=10;
     double pts[num_points*PNP];
     for (int i=0; i<num_points*PNP; i++)
-        pts[i]=i;
+        pts[i]=double(rand())/RAND_MAX;
+    // increase Z values
+    for (int i=2; i<num_points*PNP; i+=3)
+        pts[i]+=1.0;
 
     shape->reset(new RigidShape<double>(pts, num_points));
 
